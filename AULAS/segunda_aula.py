2402b6c291147ba69f2a3c96ce6a725b17c5fd1a
Lucas Bento Tomaz <olucastomaz@hotmail.com>
2023-05-08 23:50:37 -0300

    Primeiras aulas e lab

---
+++ AULAS/segunda_aula.py
@@ -0,0 +1,29 @@
+print("olá, mundo")
+print()
+print(2)
+
+
+print("Olá, Python!")
+
+
+print("A pequenina aranha \n escalou a tromba d'água.")
+print("Caiu a chuva e \n lavou a aranha.")
+
+
+print("Meu nome é", "Python.", end=" ")
+print("Monty Python.")
+print("Meu", "nome", "é", sep="_", end="*")
+
+
+
+
+
+
+print(" *")
+print(" * *")
+print(" * *")
+print(" * *")
+print("*** ***")
+print(" * *")
+print(" * *")
+print(" *****")
---
+++ LAB/LAB-Variavies.py
@@ -0,0 +1,9 @@
+John = 3
+Mary = 5
+Adam = 6
+
+print (John, Mary, Adam)
+
+total_macas = John + Mary + Adam
+
+print("Número total de maçãs:", total_macas)
