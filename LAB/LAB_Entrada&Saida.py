3e0ab7324c283514f54665976ad6b11d1811e92c
Lucas Bento Tomaz <olucastomaz@hotmail.com>
2023-05-10 22:44:57 -0300

    Exemplo simples de entrada e saida

---
+++ LAB/LAB_Entrada&Saida.py
@@ -0,0 +1,11 @@
+A = float(input("Insira um valor para A: "))
+B = float(input("Insira um valor para B: "))
+
+
+print("O resultado da adição é:" + str(A+B))
+print("O resultado da subtração é:" + str(A-B))
+print("O resultado da multiplicação é:" + str(A*B))
+print("O resultado da divisão é:" + str(A/B))
+
+
+print("\nIsso é tudo, pessoal!")
